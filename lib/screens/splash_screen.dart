5e13086f319e40cb7c8e3b77f9a82b365e0f1dc9
pnkaj1999 <pankajgit525@gmail.com>
2025-03-13 14:47:23 +0530

    18-03-2025

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,37 +1,38 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:flutter/material.dart';
 import 'package:youtube_clone/screens/home_page.dart';
 
-class SplashScreen extends StatefulWidget {
-  @override
-  SplashScreenState createState() => SplashScreenState();
-}
 
-class SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => YouTubeHome()),
-      );
-    });
-  }
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 3),(){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> YouTubeHome()));
+    });
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Center(
-            child: Image.asset('assets/youtube logo.png', width: 150),
-          ),
-          Text('YouTube Clone')
-        ],
+      body: Padding(
+        padding: const EdgeInsets.only(top: 400),
+        child: Column(
+          children: [
+            Center(
+              child: Container(
+                height: 200,
+                width: 200,
+                decoration: const BoxDecoration(
+                  // color: Colors.red,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/youtube logo.png'),
+                  ),
+                ),
+              ),
+            ),
+            const Text("YouTube Clone",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+          ],
+        ),
       ),
     );
   }
 }
-
-
